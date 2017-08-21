533455f94e6263fe9eff2600cd08a0f48a13514a
Allison Shaw <shaw.allison91@gmail.com>
2017-08-21 17:16:51 -0600

    lunch lady not working

--- lunch_lady/lunch.rb
+++ lunch_lady/lunch.rb
@@ -2,26 +2,42 @@ require 'pry'
 
 @main_items = [
 
-  { main: "Chicken", cost: 5.00 },
-  { main: "Tacos", cost: 4.00 },
-  { main: "Pizza", cost: 3.00 },
-  { main: "Steak", cost: 8.00 }
+  { main: "Chicken", price: 5.00 },
+  { main: "Tacos", price: 4.00 },
+  { main: "Pizza", price: 3.00 },
+  { main: "Steak", price: 8.00 }
   
 ]
 
 @side_items = [
 
-{ side: "Salad", cost: 1.50 },
-{ side: "Fries", cost: 1.00 },
-{ side: "Veggies", cost: 1.75 },
-{ side: "Chips", cost: 0.50 },
+{ side: "Salad", price: 1.50 },
+{ side: "Fries", price: 1.00 },
+{ side: "Veggies", price: 1.75 },
+{ side: "Chips", price: 0.50 },
 
 ]
+
+@user_items = []
+
+def menu
+  puts "Welcome to the Cafeteria!"
+  puts "Please select one main entree."
+  main_items
+  puts "What would you like for your first side?"
+  side_one
+  puts "What other side would you like?"
+  side_two
+  puts "So you would like?:"
+  order
+  puts "Your total comes to:"
+  total
+end
 
 def main_items
   puts 'Please choose your main dish.'
   @main_items.each_with_index do |key, index|
-   puts "#{index + 1}. #{key[:main]} = #{key[:cost]}"
+   puts "#{index + 1}. #{key[:main]} = #{key[:price]}"
    end
  input = gets.strip.to_i
  case input
@@ -33,7 +49,7 @@ end
 def side_items
   puts 'Please choose from the sides'
   @side_items.each_with_index do |key, index|
-    puts "#{index + 1}. #{key[:side]} = #{key[:cost]}"
+    puts "#{index + 1}. #{key[:side]} = #{key[:price]}"
     end
  input = gets.strip.to_i
  case input
@@ -54,8 +70,19 @@ def side_two
   puts "Now lets get you a total so you know what you owe"
 end
 
+def order
+  @user_items.each_with_index do |item, index|
+    index = index + 1
+    puts "#{index}. #{item[:name]} $#{item[:price]}"
+  end
+end
+
 def total
-  puts "Your total is: $#{@main_cost + @side_one.cost + @side_two.cost}"
+  sum = 0.0
+  @user_items.each do |item|
+    sum += item[:price]
+  end
+  puts "Your total is: $#{sum.round(2)}"
 end
 
 main_items
